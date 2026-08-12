40daedbcd61913d5977887c527fe3b418da535d0
Sharath Nayak <sharathmnayak2005@gmail.com>
2026-08-12 22:05:32 +0530

    add soft_constraints

---
+++ 05_randomization_and_constraints/soft_constraints.sv
@@ -0,0 +1,24 @@
+// Soft Constraints and Inline Constraint Overriding (with {})
+class seq_item;
+
+  rand bit [7:0] val;
+  constraint val_c { soft val inside {5, [10:15]}; }
+
+endclass
+
+module soft_constraints_tb;
+
+  seq_item item;
+
+  initial begin
+    item = new();
+    repeat (3) begin
+      item.randomize();
+      $display("Before inline constraint: val = %0d", item.val);
+
+      item.randomize() with { val inside {[20:30]}; };
+      $display("After inline constraint : val = %0d", item.val);
+    end
+  end
+
+endmodule
